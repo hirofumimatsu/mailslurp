7b66b4bdf50a17cb21d6cdca0bad0e2c51cd41ed
Hirofumi MATSUMOTO <hirofumimatsu@UG067-hirofu5.local>
2021-08-08 22:56:01 +0900

    pg_queueの追加

---
+++ receive-mail/haraka_test/plugins/pg_queue.js
@@ -0,0 +1,6 @@
+// pg_queue
+
+// documentation via: haraka -c /Users/hirofumimatsu/work/mailslurp/receive-mail/haraka_test -h plugins/pg_queue
+
+// Put your plugin code here
+// type: `haraka -h Plugins` for documentation on how to create a plugin
