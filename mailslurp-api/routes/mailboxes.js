7b4f3da85fa20558253880ce27d7736f0648bf11
Hirofumi MATSUMOTO <hirofumimatsu@UG067-hirofu5.local>
2021-08-19 21:43:40 +0900

    メールボックス関連APIの作成

--- mailslurp-api/routes/mailboxes.js
+++ mailslurp-api/routes/mailboxes.js
@@ -22,6 +22,7 @@ const buildAPIServer = () => {
   }
 };
 
+// メール一覧の応答を作成
 const buildMailboxesResponse = (mailboxes) => {
   let mailboxArray = [];
   const server = buildAPIServer();
@@ -37,49 +38,113 @@ const buildMailboxesResponse = (mailboxes) => {
   return mailboxArray;
 };
 
-// メールボックス一覧表示
-router.get("/", async function (req, res, next) {
-  const mailboxes = await prisma.mailbox.findMany();
-  const response = {
-    mailboxes: buildMailboxesResponse(mailboxes),
-  };
-  res.status(StatusCodes.OK).send(response);
+// メールボックス詳細表示
+router.get("/:mailboxId", async function (req, res, next) {
+  const server = buildAPIServer();
+  const mailbox = prisma.mailbox
+    .findUnique({
+      where: { id: parseInt(req.params.mailboxId) },
+    })
+    .then((mailbox) => {
+      if (mailbox) {
+        res.status(StatusCodes.OK).send({
+          mailboxId: mailbox.id,
+          email: mailbox.email,
+          link: server + "mailboxes/" + mailbox.id,
+        });
+      } else {
+        res.status(StatusCodes.NOT_FOUND).send();
+      }
+    })
+    .catch((error) => {
+      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+        error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+      });
+    });
 });
 
-const createMailbox = async (mailboxId) => {
-  const newMailbox = await prisma.mailbox.create({
-    data: {
-      id: mailboxId,
-      email: mailboxId + "@" + emailDomain,
-      userId: 1,
-    },
-  });
-};
-
-// メールボックス作成
-router.put("/", function (req, res, next) {
-  const mailboxId = uuidv4();
-  if (createMailbox(mailboxId)) {
-    res.status(StatusCodes.CREATED).send({
-      mailboxId: mailboxId,
-      email: mailboxId + "@" + emailDomain,
-      link: server + "mailboxes/" + mailboxId,
-    });
-  } else {
+// メールボックス一覧表示
+router.get("/", async function (req, res, next) {
+  try {
+    const mailboxes = await prisma.mailbox.findMany();
+    const response = {
+      mailboxes: buildMailboxesResponse(mailboxes),
+    };
+    res.status(StatusCodes.OK).send(response);
+  } catch (error) {
     res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
       error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
     });
   }
 });
 
-// メールボックス詳細表示
-router.get("/:id", function (req, res, next) {
-  res.send("respond with a resource");
+const createMailbox = async () => {
+  const uuid = uuidv4();
+  const newMailbox = await prisma.mailbox.create({
+    data: {
+      email: uuid + "@" + settings.emailDomain,
+      userId: 1,
+    },
+  });
+  return newMailbox;
+};
+
+// メールボックス作成
+router.post("/", async function (req, res, next) {
+  const uuid = uuidv4();
+  const server = buildAPIServer();
+
+  try {
+    const newMailbox = await createMailbox();
+    res.status(StatusCodes.CREATED).send({
+      mailboxId: newMailbox.id,
+      email: newMailbox.email,
+      link: server + "mailboxes/" + newMailbox.id,
+    });
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
+  }
 });
 
 // メールボックス削除
-router.delete("/:id", function (req, res, next) {
-  res.send("respond with a resource");
+router.delete("/:mailboxId", async function (req, res, next) {
+  const mailboxId = parseInt(req.params.mailboxId);
+  try {
+    const mailbox = await prisma.mailbox.findUnique({
+      where: { id: mailboxId },
+    });
+    if (mailbox) {
+      await prisma.mailbox
+        .update({
+          where: { id: mailboxId },
+          data: {
+            messages: {
+              deleteMany: {},
+            },
+          },
+        })
+        .then(async (mailbox) => {
+          await prisma.mailbox
+            .delete({
+              where: { id: mailboxId },
+            })
+            .then((mailbox) => {
+              res.status(StatusCodes.OK).send();
+            });
+        })
+        .catch((error) => {
+          res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+        });
+    } else {
+      res.status(StatusCodes.NOT_FOUND).send();
+    }
+  } catch (error) {
+    res.status(StatusCodes.INTERNAL_SERVER_ERROR).send({
+      error: getReasonPhrase(StatusCodes.INTERNAL_SERVER_ERROR),
+    });
+  }
 });
 
 module.exports = router;
